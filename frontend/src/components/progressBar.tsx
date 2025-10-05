326307179904bdd673af36d50f0fad7aa4725305
Esha Vigneswaran <eshavigneswaran@gmail.com>
2025-10-05 12:27:54 -0400

    Added progress bar

--- frontend/src/components/progressBar.tsx
+++ frontend/src/components/progressBar.tsx
@@ -1,11 +1,11 @@
 export default function ProgressBar({ page, total }: { page: number; total: number }) {
   return (
-    <div className="flex items-center gap-2 text-[#D9D9D9]">
+    <div className="flex items-center gap-2 text-[#D9D9D9] p-[5px]">
       {Array.from({ length: total }, (_, i) => (
         <div
           key={i}
           className={`w-[10px] h-[10px] rounded-full ${
-            i + 1 === page ? 'bg-blue-600' : 'bg-[#D9D9D9]'
+            i + 1 === page ? 'bg-gradient-to-b from-[#262633] to-[#727299]' : 'bg-[#D9D9D9]'
           }`}
         />
       ))}
