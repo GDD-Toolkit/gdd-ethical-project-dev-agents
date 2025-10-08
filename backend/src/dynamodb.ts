6924c193d7cf52218b19aab2e5577b2b4e3f3959
krishnan mac <inbantholkappian@gmail.com>
2025-10-08 11:01:43 -0400

    created basic functions

--- backend/src/dynamodb.ts
+++ backend/src/dynamodb.ts
@@ -1,11 +1,13 @@
 import { config } from "dotenv";
 import { DynamoDBClient, ListTablesCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand, PutCommandOutput} from "@aws-sdk/lib-dynamodb";
 
 config();
 
 const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
 
-export async function getTables() {
+export async function getTables(): Promise<void> {
   try {
     const command = new ListTablesCommand({});
     const response = await client.send(command);
@@ -17,4 +19,27 @@ export async function getTables() {
   }
 }
 
-getTables();
+export async function sendCommand(command) {
+  try {
+    const response = await docClient.send(command);
+    return response;
+  } catch (error) {
+    console.error("Error sending command:", error);
+    throw error;
+  }
+}
+
+export async function addItem<T extends Record<string, any>>(item: T, tableName: string): Promise<PutCommandOutput> {
+  try {
+    const command = new PutCommand({
+      TableName: tableName,
+      Item: item,
+    });
+
+    const response = await sendCommand(command);
+    return response as PutCommandOutput;
+  } catch (error) {
+    console.error("Error adding item:", error);
+    throw error;
+  }
+}
